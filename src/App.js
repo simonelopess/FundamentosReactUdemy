c537aca1e051a2a6564ae6fdae16e63f52f1bb11
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2021-01-05 21:38:15 -0300

    Using props to style a component

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ const App = () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
-        <Card titulo="#03 -Exemplo de Card">
-          <RandomComponent max={60} min={1} title="Números Aleatórios" />
+        <Card titulo="#03 -Exemplo de Card" color="#FA6900">
+          <RandomComponent max={60} min={1} title="Números Aleatórios"/>
         </Card>
-        <Card titulo="#02 -Second component">
+        <Card titulo="#02 -Second component" color="#92B06A">
           <WithParams
             title="Second component"
             subtitle="Very cool"
             grade={9.3}
           />
         </Card>
-        <Card titulo="#01 - Exemplo Primeiro Component">
+        <Card titulo="#01 - Exemplo Primeiro Component" color="#FFAC00">
           <First />
         </Card>
       </div>
