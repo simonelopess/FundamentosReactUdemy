eb317a4e2ef43d4b11796845543fd86d51a5b91a
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2021-01-05 21:21:30 -0300

    WIP - Styling card component (flexbox)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import First from './components/basics/First';
 import WithParams from './components/basics/WithParams';
 import RandomComponent from './components/basics/RandomComponent';
 import Card from './components/layout/Card';
-import './index.css';
-
+import './App.css'
 const App = () => {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
-      <Card titulo="#03 -Exemplo de Card">
-        <RandomComponent max={60} min={1} title="Números Aleatórios"/>
-      </Card>
-      <Card titulo="#02 -Second component"> 
-        <WithParams
-          title="Second component"
-          subtitle="Very cool"
-          grade={9.3}
-        />
-      </Card>
-      <Card titulo="#01 - Exemplo Primeiro Component">
-        <First />
-      </Card>
+      <div className="Cards">
+        <Card titulo="#03 -Exemplo de Card">
+          <RandomComponent max={60} min={1} title="Números Aleatórios" />
+        </Card>
+        <Card titulo="#02 -Second component">
+          <WithParams
+            title="Second component"
+            subtitle="Very cool"
+            grade={9.3}
+          />
+        </Card>
+        <Card titulo="#01 - Exemplo Primeiro Component">
+          <First />
+        </Card>
+      </div>
     </div>
   )
 }
