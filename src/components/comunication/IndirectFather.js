966ac9e3738e7d999a89ad5c1d4f7b10133e58a6
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2021-01-06 17:51:35 -0300

    Working with State

--- src/components/comunication/IndirectFather.js
+++ src/components/comunication/IndirectFather.js
@@ -1,16 +1,25 @@
+import {useState} from 'react';
+
 import IndirectChild from './IndirectChild';
 
 const IndirectFather = (props) => {
 
+  const [name, setName] = useState('?');
+  const [age, setAge] = useState(0);
+  const [isNerd, setIsNerd] = useState(false);
   //nome idade nerd
 
-  function getInformations(name, age, isNerd) {
-    console.log(name, age, isNerd);
+  function getInformations(name, age, nerd) {
+    setName(name)
+    setAge(age)
+    setIsNerd(nerd)
   }
 
   return (
     <div>
-      Pai
+      <span>{name} </span>
+      <span>{age} </span>
+      <span>{isNerd ? 'Verdadeiro' : 'Falso'}</span>
       <IndirectChild click={getInformations}/>
     </div>
   )
